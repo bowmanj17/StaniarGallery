99d796a3d3480079bc8851a4596b70778e5cd8c0
bowmanj17 <bowmanj17@mail.wlu.edu>
2017-03-26 14:33:20 -0400

    Add in picture frame and VRTK assets

---
+++ StaniarGallery/Assets/VRTK/Examples/Resources/Scripts/Whirlygig.cs
@@ -0,0 +1,33 @@
+﻿namespace VRTK.Examples
+{
+    using UnityEngine;
+
+    public class Whirlygig : VRTK_InteractableObject
+    {
+        float spinSpeed = 60f;
+        Transform rotator;
+
+        public override void StartUsing(GameObject usingObject)
+        {
+            base.StartUsing(usingObject);
+            spinSpeed = 60f;
+        }
+
+        public override void StopUsing(GameObject usingObject)
+        {
+            base.StopUsing(usingObject);
+            spinSpeed = 0f;
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+            rotator = transform.Find("Capsule");
+        }
+
+        protected override void Update()
+        {
+            rotator.transform.Rotate(new Vector3(0f, spinSpeed * Time.deltaTime, 0f));
+        }
+    }
+}
